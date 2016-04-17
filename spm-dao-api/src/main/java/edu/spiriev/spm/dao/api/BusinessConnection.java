37017cae606032a942e6b4100a1462629f60a266
spiriev <gspiriev@gmail.com>
2016-04-17 19:11:41 +0300

    stable with DB

---
+++ spm-dao-api/src/main/java/edu/spiriev/spm/dao/api/BusinessConnection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.spiriev.spm.dao.api;
+
+/**
+ *
+ * @author root_spiriev
+ */
+public interface BusinessConnection {
+    
+    void commitTransaction();
+    
+}
